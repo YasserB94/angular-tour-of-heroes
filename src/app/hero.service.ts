4df7f2b2f9e4644a09f45350caa43b961ef23f9a
Yasser Bal <yasserbal@icloud.com>
2022-08-07 10:28:30 +0200

    Now uses Messages Service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,22 @@
+//ANGULAR
 import { Injectable } from '@angular/core';
-
+//RXJS
 import { Observable, of } from 'rxjs';
-
+//SERVICES
+import { MessageService } from './message.service';
+//INTERFACES
 import { Hero } from './hero';
+//DATA
 import { HEROES } from './mock-heroes';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService:MessageService) { }
   getHeroes():Observable<Hero[]>{
     const heroes= of(HEROES);
+    this.messageService.add('HeroService: fetched heroes')
     return heroes;
   }
 }
